8b1797e3a0dcbc38f988b842601cc339f094c142
Sergey_Utkin <sergey.uty.uty@gmail.com>
2022-05-08 20:43:37 +0300

    Добавьте файлы проекта.

---
+++ bsol33.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int N = 0, sum = 0;
+    cout << "Введите N-число \n";
+    cin >> N;
+    cout << "Введите N-чисел:\n";
+    do
+    {
+        int temp;
+        cin >> temp;
+        if (temp > 0)
+        {
+            sum += temp;
+        }
+        N -= 1;
+    } while (N > 0);
+    cout << "Сумма положительных чисел: " << sum;
+}
