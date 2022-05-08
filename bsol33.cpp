97e3c1275d336a1df1a65176fbfd2e13fc12eb4f
Sergey_Utkin <sergey.uty.uty@gmail.com>
2022-05-08 23:53:11 +0300

    ad

--- bsol33.cpp
+++ bsol33.cpp
@@ -5,21 +5,28 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    int N = 0, sum = 0, cnt = 0;
-    cout << "Введите N-число \n";
-    cin >> N;
-    cout << "Введите N-чисел:\n";
-    do
+    int myArray[15], sum = 0;
+    string ans;
+    cout << "Заполнить массив вручную - 1 \n Заполнить массив случайными числами - 2 \n";
+    cin >> ans;
+    if (ans == "1")
     {
-        int temp;
-        cin >> temp;
-        if (temp > 0)
+        for (int i = 0; i < size(myArray); i++)
         {
-            cnt += 1;
-            sum += temp;
+            cout << " ";
+            cin >> myArray[i];
+            sum+= myArray[i];
         }
-        N -= 1;
-    } while (N > 0);
-    
-    cout << "Сумма положительных чисел: " << (double)sum / (double)cnt;
+    }
+    else
+    {
+        srand(time(0));
+        for (int i = 0; i < size(myArray); i++)
+        {
+            myArray[i] = rand();
+            cout << " " << myArray[i];
+            sum += myArray[i];
+        }
+    }
+    cout << "\n Среднее арифметическое элементов: " << sum / 15.0;
 }
